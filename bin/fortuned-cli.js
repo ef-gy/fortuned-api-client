7ab453b3b484b4cc7a3d77416a9ffbb1cbf23d89
Magnus Deininger <magnus@ef.gy>
2017-06-08 09:40:28 +0000

    adopt the commander package and add CLI options for requesting a specific cookie as well as printing the sourc e information on the retrieved cookies.

--- bin/fortuned-cli.js
+++ bin/fortuned-cli.js
@@ -1,6 +1,28 @@
 #!/usr/bin/nodejs
 
-require('../fortuned-api-client').cookie().
-  then(function(cookie) {
-    process.stdout.write(cookie.cookie);
-  }, console.warn);
+var cookie = require('../fortuned-api-client').cookie,
+    commander = require('commander'),
+    meta = require('../package.json');
+
+commander
+  .version(meta.version)
+  .option('-c, --id <n>', 'get specific cookie', parseInt)
+  .option('-s, --source','print the source of the cookie')
+  .option('-i, --show', 'print the cookie id, for use with --id')
+  .parse(process.argv);
+
+cookie(commander.id).then(function(cookie) {
+      var extraNewline = commander.source || commander.show;
+      if (extraNewline) {
+        console.log(cookie.cookie);
+      } else {
+        process.stdout.write(cookie.cookie);
+      }
+
+      if (commander.source) {
+        console.log('source file: %s#%d', cookie.file, cookie['file-id']);
+      }
+      if (commander.show) {
+        console.log('cookie ID: %d', cookie.id);
+      }
+    }, console.warn);
