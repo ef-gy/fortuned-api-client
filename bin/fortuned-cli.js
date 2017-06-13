a1eacb51edb391a5752868c59cef3d7715b2c653
Magnus <magnus@ef.gy>
2017-06-13 06:18:34 -0400

    some restructuring, add a Cookie() object, add a manpage, install the CLI wrapper without the .js suffix.

--- bin/fortuned-cli.js
+++ bin/fortuned-cli.js
@@ -8,11 +8,18 @@ const meta = require('../package.json');
 
 commander
   .version(meta.version)
+  .option('-o, --offensive', 'show a (potentially) offensive cookie')
   .option('-c, --file', 'show the source file that the cookie came from')
+  .option('--endpoint [address]',
+          'select the API endpoint [https://api.ef.gy]',
+          'https://api.ef.gy')
   .option('--id <n>', 'get specific cookie', parseInt)
   .option('--show', 'print the cookie id, for use with --id')
   .option('--source', 'print the source of the cookie, with in-file id')
   .parse(process.argv);
+
+const api = new cookie.Api(commander.endpoint);
+let retries = 0;
 
 /**
  * @brief Display the returned cookie.
@@ -20,28 +27,39 @@ commander
  * We have this invoked by the fortune cookie API's promise for when we do
  * successfully query a cookie.
  *
- * @param {Object} cookie A JSON object with fortune cookie data.
+ * @param {Object} data A JSON object with fortune cookie data.
  */
-function withCookie(cookie) {
+function withCookie(data) {
+  let co = new cookie.Cookie(data);
+  let offensive = commander.offensive || false;
+
+  if (co.isOffensive() != offensive) {
+    if (retries < 50) {
+      retries++;
+      api.fortune(commander.id).then(withCookie, console.warn);
+    } else {
+      console.warn('Sorry, I couldn\'t find what you are looking for.');
+    }
+    return;
+  }
+
   if (commander.file) {
-    console.log('(%s)\n%%', cookie.file);
+    console.log('(%s)\n%%', co.file);
   }
 
   const extraNewline = commander.source || commander.show;
   if (extraNewline) {
-    console.log(cookie.cookie);
+    console.log(co.cookie);
   } else {
-    process.stdout.write(cookie.cookie);
+    process.stdout.write(co.cookie);
   }
 
   if (commander.source) {
-    console.log('source file: %s#%d', cookie.file, cookie['file-id']);
+    console.log('source file: %s#%d', co.file, co.fileID);
   }
   if (commander.show) {
-    console.log('cookie ID: %d', cookie.id);
+    console.log('cookie ID: %d', co.id);
   }
 }
-
-const api = new cookie.Api();
 
 api.fortune(commander.id).then(withCookie, console.warn);
--- index.js
+++ index.js
@@ -2,4 +2,5 @@
 
 module.exports = {
   'Api': require('./lib/api.js'),
+  'Cookie': require('./lib/cookie.js'),
 };
